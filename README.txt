b8e698eb964be62ab9c698eee2521b0be2deb190
Michiko Inaba <michiko.luther@gmail.com>
2017-03-01 11:37:41 -0800

    added new logic to create a pie chart, pdf file, and excel report for rented tools.

--- README.txt
+++ README.txt
@@ -2,6 +2,9 @@ reference:
 https://zellwk.com/blog/crud-express-mongodb/
 https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
 
+angular-chart
+http://jtblin.github.io/angular-chart.js/
+
 To start the site
 node server.js
 
--- public/js/controllers/rentedToolsCtrl.js
+++ public/js/controllers/rentedToolsCtrl.js
@@ -3,7 +3,7 @@ angular.module('rentedToolsCtrl', [])
 
 //inject the Todo service factory into our controller
 .controller('rentedToolsCtrl',['$scope','$http','rented_tools', function($scope, $http,rented_tools ) {
-	
+
 	//get all rented_date from then rented_tools table
 	rented_tools.getdate()
 	.success(function(data) {
@@ -58,7 +58,7 @@ angular.module('rentedToolsCtrl', [])
 	    
 	});
 	
-	
+
 	 $scope.tools='';
 	
 	 //get the select list value and populate the month's data
@@ -128,14 +128,14 @@ angular.module('rentedToolsCtrl', [])
 			
 			//get all tools ids.
 			rented_tools.gettool($scope.selectedMonth.operator) 
-			.success(function(data) {
+			.success(function(response) {
 			
 				
 			//generate json string
-			  var json_string=jQuery.parseJSON(data);
+			  var json_string=jQuery.parseJSON(response);
 			  var allTools = json_string;
 			  
-			console.log(JSON.stringify(data));
+			//console.log(JSON.stringify(data));
 			  //loop thru tools obj and id obj.
 		        //get the number of tool ids that was rented.
 		        var len = tool_ids.length;
@@ -158,7 +158,7 @@ angular.module('rentedToolsCtrl', [])
 		           
 		           
 		        	}//for (toolProp in allTools) {
-		        	allTools.push(counter);
+		        	data.push(counter);
 					
 		        }//  for (var i = 0; i < len; i++) {
 		      
@@ -178,13 +178,14 @@ angular.module('rentedToolsCtrl', [])
 		   
 		        }//  for(var j = 0; j < data_len; j++) {
 		        
-		    	//console.log(JSON.stringify(labels));
+		    	//console.log(labels);
 				//console.log(JSON.stringify(allTools));
+			
 		    	//labels
 				$scope.labels = labels;
 				
 				//chart data
-				$scope.data = allTools;
+				$scope.data = data;
 				
 				//show legend
 				$scope.options = {legend: {display: true}};
@@ -199,6 +200,126 @@ angular.module('rentedToolsCtrl', [])
 		
 	
 	}//$scope.showChart = function(){	
+	
+	//create a PDF file
+	$scope.createPDF = function(){
+		
+		//get html2canvas elements
+		//var month = PrintCanvas('month');
+		var chart = PrintCanvas('pieChart');
+		var table = PrintCanvas('exportable');
+	    var month = $scope.selectedMonth.operator;
+	    
+		//console.log('chart '+chart+'$scope.selectedMonth.operator, '+$scope.selectedMonth.operator);
+		//check if a month is selected. if not, show the error message
+		if($scope.selectedMonth.operator =='selectmonth'){
+			
+			Materialize.toast('Month is not selected', 4000);
+		
+			//check if the pie chart is hidden. If so, remove the chart element from docDefinition to generate a PDF file.
+		}else if($scope.showPieChart ==false){
+			
+			var docDefinition = {
+					
+	                  content: [
+	                    {
+	                    	text:month,
+	                    	fontSize: 12 
+	                    	
+	                    },
+	                   {  
+	                	  image:table,
+	                	  width: 500,
+	                	  
+	                   }]
+				}//var docDefinition = {
+				
+			  //generate a pdf file	
+			  pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
+			
+		}else{
+			
+			var docDefinition = {
+					
+	                  content: [
+	                   {
+			              text:month,
+			              fontSize: 12 
+			                    	
+			           }, 
+	                   {
+	                  	allowTaint:true,
+	                      image: chart,
+	                      width: 400,
+	                      
+	                  },{
+	                	  image:table,
+	                	  width: 500,
+	                	  
+	                  }]
+				}//var docDefinition = {
+				
+			  //generate a pdf file	
+			  pdfMake.createPdf(docDefinition).download(month+"_rented_tools.pdf");
+			  
+		}//else
+		
+	  //this creates html2canvas html element data.
+	  function PrintCanvas(divId) {
+            //div canvas
+            var divObj = html2canvas($('#' + divId));
+            var divQueu = divObj.parse();
+            var divCanvas = divObj.render(divQueu);
+            divImg = divCanvas.toDataURL();
+
+            return divImg;
+        }//function PrintCanvas(divId) { 
+	  
+	  
+	  
+	}//$scope.createPDF = function(){
+	
+	
+	
+	//generate a xls file
+	$scope.createReport = function(){
+		var tools='';
+	
+		//get all tools ids.
+	    if($scope.selectedMonth.operator==''){
+	    	Materialize.toast('Please select a month.', 4000);
+	    	
+	    }else{
+	     
+			rented_tools.gettool($scope.selectedMonth.operator) 
+			.success(function(response) {
+				//generate json string
+				var json_string=jQuery.parseJSON(response);
+					$scope.tools = json_string;
+		        //get today's date for the file name
+			    var d = new Date();
+			    var curr_date = d.getDate();
+			    var curr_month = d.getMonth() + 1; //Months are zero based
+			    var curr_year = d.getFullYear();
+			    var today = curr_month + "-" + curr_date + "-" + curr_year;
+			   
+	    
+			    //generate the spread sheet with the html table data value
+				var blob = new Blob([document.getElementById('exportable').innerHTML], {
+			         type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=utf-8"
+			    });
+			    
+				 saveAs(blob, "RentedToolReport-"+today+".xls");
+		       
+		    })
+		       
+			.error(function(data, status, headers, config){
+				Materialize.toast('Unable to retrieve record.', 4000);
+			 });	
+	   
+	    }
+		
+	}//$scope.createReport = function(){
 	
 }]);//.controller('
 
